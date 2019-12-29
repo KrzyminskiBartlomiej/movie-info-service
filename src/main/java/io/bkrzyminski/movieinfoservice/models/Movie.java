b190fcfa56bc8fb6f3069da0f3ab8b67cbc26457
bkrzyminski <krzyminski.bartlomiej@gmail.com>
2019-12-29 20:15:22 +0100

    Added port property, models and resources

---
+++ src/main/java/io/bkrzyminski/movieinfoservice/models/Movie.java
@@ -0,0 +1,28 @@
+package io.bkrzyminski.movieinfoservice.models;
+
+public class Movie {
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String movieId;
+    private String name;
+}
---
+++ src/main/java/io/bkrzyminski/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,16 @@
+package io.bkrzyminski.movieinfoservice.resources;
+
+import io.bkrzyminski.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId){
+        return new Movie(movieId, "Test name");
+    }
+}
